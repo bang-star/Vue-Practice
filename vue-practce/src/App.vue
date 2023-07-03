9ca462859e52e4389b78941a3a17296c570895d0
bang-star <bang77060986@gmail.com>
2023-07-04 00:56:51 +0900

    #3 - Component

--- vue-practce/src/App.vue
+++ vue-practce/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world msg="Welcome to Your Vue.js App" @click-msg="clickMsg"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  methods: {
+    clickMsg() {
+      console.log('click')
+    }
   }
 }
 </script>
