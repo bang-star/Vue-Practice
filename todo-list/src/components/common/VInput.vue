45ae32841e3c70fa8af24cef49e69eff2f6d609d
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-07 01:50:01 +0900

    #15 - 공통 컴포넌트(Input) (#16)

---
+++ todo-list/src/components/common/VInput.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="v-input">
+    <input :type="type"
+           :value="value"
+           @input="change"
+           class="v-input__text"
+           :placeholder="placeholder" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VInput',
+  props: {
+    type: {
+      type: String,  // text, password, number
+      default: 'text'
+    },
+    value: {
+      type: String,
+    },
+    placeholder: {
+      type: String
+    }
+  },
+  methods: {
+    change(event) {
+      this.$emit('change', event.target.value);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-input {
+  display: inline-block;
+}
+.v-input__text {
+  width: 120px;
+  height: 30px;
+
+  outline: none;
+  border: 1px solid rgba(0, 0, 0, .15);
+  padding-left: 8px;
+  margin-right: 8px;
+
+  &:hover {
+    border-color: rgba(0,0,0,.54);
+  }
+
+  &:focus {
+    border-color: #00c4c4;
+  }
+}
+
+</style>
