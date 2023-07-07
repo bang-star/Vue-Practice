ee12964756616cdc994e01f424cd2913c524e1a2
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-08 02:20:40 +0900

    #19 - 리팩토링 (#20)

--- todo-list/src/views/HomeView.vue
+++ todo-list/src/views/HomeView.vue
@@ -3,27 +3,13 @@
     <div class="container">
       <h2 class="title">할 일 추가 앱</h2>
       <div class="add-area">
-        <v-input v-model="todo" />
+        <v-input v-model="todo"
+                 @enter="addTodo"
+                 class="add-area__button"/>
         <v-button @click="addTodo" variant="outlined">추가</v-button>
       </div>
-      <ul class="todo-list">
-        <li class="todo-item" v-for="(item, index) in todoList" :key="index">
-          <div class="todo-area">
-            <input class="todo-item__status" type="checkbox" v-model="item.isChecked">
-            <span v-if="!item.isUpdate" :class="{ isFinish: item.isChecked }">
-              {{ item.name }}
-            </span>
-            <span v-else>
-              <input class="todo-item__text" type="text" v-model="item.name" placeholder="할 일을 적어주세요.">
-            </span>
-          </div>
-          <div class="button-area">
-            <font-awesome-icon v-if="!item.isUpdate" :icon="['fas', 'pen']" @click="item.isUpdate = true"  />
-            <font-awesome-icon v-else :icon="['fas', 'check']" @click="item.isUpdate = false" />
-            <font-awesome-icon :icon="['fass', 'trash']"  @click="removeItem(index)" />
-          </div>
-        </li>
-      </ul>
+      <todo-list :todoList="todoList"
+                 @remove="removeItem"/>
     </div>
   </div>
 </template>
@@ -31,12 +17,14 @@
 <script>
 import VInput from "@/components/common/VInput.vue";
 import VButton from "@/components/common/VButton.vue";
+import TodoList from "@/components/todo/TodoList.vue";
 
 export default {
   name: 'HomeView',
   components: {
     VInput,
     VButton,
+    TodoList
   },
   data() {
     return {
@@ -70,6 +58,7 @@ export default {
       this.todo = '';
     },
     removeItem(idx) {
+      console.log(idx)
       this.todoList = this.todoList.filter((item, index) => index !== idx);
     },
   }
@@ -101,56 +90,10 @@ export default {
   display: flex;
   align-items: center;
   justify-content: end;
-}
 
-
-
-.todo-list {
-  width: 100%;
-}
-
-.todo-item {
-  width: 100%;
-  height: 30px;
-  border-bottom: 1px solid gray;
-  padding: 8px 0;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.button-area {
-  svg {
-    cursor: pointer;
-    color: gray;
-    margin-left: 8px;
+  &__button {
+    margin-right: 8px;
   }
 }
 
-.todo-item__text {
-  width: 150px;
-  height: 30px;
-
-  outline: none;
-  border: 1px solid rgba(0, 0, 0, .15);
-  padding-left: 8px;
-  margin-right: 8px;
-
-  &:hover {
-    border-color: rgba(0, 0, 0, .54);
-  }
-
-  &:focus {
-    border-color: #00c4c4;
-  }
-}
-
-.todo-item__status {
-  margin-right: 8px;
-}
-
-.isFinish {
-  text-decoration: line-through;
-}
 </style>
