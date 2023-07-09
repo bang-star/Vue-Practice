e3eb12d5c366ca301ccb9818428da08fcafd7a72
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-09 11:58:03 +0900

    #31 - 로그인 로직 구현 (#34)

--- login-page/src/views/LoginView.vue
+++ login-page/src/views/LoginView.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="login-view">
-    <h1>Login page</h1>
-    <router-link to="/">홈으로</router-link>
+    <img src="@/assets/logo.png" alt="logo">
+    <login-form class="form" />
   </div>
 </template>
+
+<script>
+import LoginForm from "@/components/login/LoginForm.vue";
+
+export default {
+  name: 'login-view',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.login-view {
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+
+  .form {
+    width: 300px;
+  }
+}
+</style>
