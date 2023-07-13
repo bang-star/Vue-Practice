684193051ca89d18a5dd4fbf8b8fbadd22ee64c2
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-13 22:28:54 +0900

    #42 - 인증 구현 (#44)

--- login-page/src/store/index.js
+++ login-page/src/store/index.js
@@ -5,10 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    accessToken: ''
   },
   getters: {
+    isLogin(state) {
+      return state.accessToken !== '';
+    }
   },
   mutations: {
+    setLogin(state, payload) {
+      console.log(payload)
+      state.accessToken = payload;
+    }
   },
   actions: {
   },
