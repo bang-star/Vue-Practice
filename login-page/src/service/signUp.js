4f22010c3e3806270d24d905f66991b0d6417192
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-13 22:19:43 +0900

    #41 - JWT 구현 (#43)

--- login-page/src/service/signUp.js
+++ login-page/src/service/signUp.js
@@ -1,9 +1,11 @@
 import axios from "@/service/axios";
+import router from "@/router";
 
 export const signUp = async (data) => {
   try {
     const { id, password } = data;
     await axios.post('/signUp', { id, password });
+    router.push('/login');
   } catch (err) {
     console.log(err);
   }
