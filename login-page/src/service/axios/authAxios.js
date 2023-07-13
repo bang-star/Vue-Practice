50dc655ce9cdbdc9448e860e1c7ea86b801b476f
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-13 22:58:23 +0900

    #46 [인증 유지] 쿠키를 이용한 인증 (#47)

--- login-page/src/service/axios/authAxios.js
+++ login-page/src/service/axios/authAxios.js
@@ -1,12 +1,14 @@
-import axios from "axios";
+import axios from "./axios";
+import store from "@/store";
 
-axios.defaults.baseURL = 'http://localhost:3000/'
+const authAxios = axios.create();
 
 // 요청 인터셉터 추가하기
-axios.interceptors.request.use(function (config) {
+authAxios.interceptors.request.use(function (config) {
   // 요청이 전달되기 전에 작업 수행
-  
   // 로그 찍기
+  config.headers['access-token'] = store.state.accessToken;
+
   return config;
 }, function (error) {
   // 요청 오류가 있는 작업 수행
@@ -15,7 +17,7 @@ axios.interceptors.request.use(function (config) {
 });
 
 // 응답 인터셉터 추가하기
-axios.interceptors.response.use(function (response) {
+authAxios.interceptors.response.use(function (response) {
   // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
   // 응답 데이터가 있는 작업 수행
   return response;
@@ -25,4 +27,4 @@ axios.interceptors.response.use(function (response) {
   return Promise.reject(error);
 });
 
-export default axios;
+export default authAxios;
