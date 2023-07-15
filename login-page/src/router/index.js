25d00287cca9e7d40307208593f855a4f491bb86
bang-star <bang77060986@gmail.com>
2023-07-15 21:44:53 +0900

    #54 - 인증 유지 성능 개선

--- login-page/src/router/index.js
+++ login-page/src/router/index.js
@@ -1,16 +1,21 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
 import SignUpView from "@/views/SignUpView.vue";
 // import store from "@/store";
 
-Vue.use(VueRouter)
+import { extendLoginTime } from "@/service/login";
+
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
+    meta: {
+      requireAuth: true,
+    },
     // beforeEnter: (to, from, next) => {
     //   const isLogin = store.getters.isLogin;
     //   if(isLogin) {
@@ -22,24 +27,39 @@ const routes = [
     // }
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
   },
   {
-    path: '/signUp',
-    name: 'signUp',
-    component: SignUpView
-  }
-]
+    path: "/signUp",
+    name: "signUp",
+    component: SignUpView,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  console.log("router beforeEach");
+  const accessToken = Vue.$cookies.get("accessToken");
+  const refreshToken = Vue.$cookies.get("refreshToken");
+
+  if (to.meta.requireAuth && !accessToken && refreshToken) {
+    await extendLoginTime();
+  } else if (to.meta.requireAuth && !refreshToken) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router
